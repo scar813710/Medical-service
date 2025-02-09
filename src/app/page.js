653b912687eda742345d26e2d23ba115aa0be3e8
scar813710 <rodolfguerr@gmail.com>
2025-02-10 05:05:45 +0900

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Login from "./(auth)/Login/page";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Login />
+    </div>
+  );
+}
